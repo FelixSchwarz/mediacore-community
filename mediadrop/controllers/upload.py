bc1dc569d26068f65a871a45a9e813c5ee9978b4
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2013-11-04 15:27:03 +0100

    rename package name from "mediacore" to "mediadrop" (similar to 6d7f9d9dd27d6910c016bc943d7ee3e1d7a36988)

---
+++ mediadrop/controllers/upload.py
@@ -0,0 +1,234 @@
+# This file is a part of MediaDrop (http://www.mediadrop.net),
+# Copyright 2009-2013 MediaDrop contributors
+# For the exact contribution history, see the git revision log.
+# The source code contained in this file is licensed under the GPLv3 or
+# (at your option) any later version.
+# See LICENSE.txt in the main project directory, for more information.
+
+import simplejson as json
+
+from pylons import request, tmpl_context
+from pylons.controllers.util import abort
+
+from mediadrop.forms.uploader import UploadForm
+from mediadrop.lib import email
+from mediadrop.lib.base import BaseController
+from mediadrop.lib.decorators import autocommit, expose, observable, validate
+from mediadrop.lib.helpers import redirect, url_for
+from mediadrop.lib.storage import add_new_media_file
+from mediadrop.lib.thumbnails import create_default_thumbs_for, has_thumbs
+from mediadrop.model import Author, DBSession, get_available_slug, Media
+from mediadrop.plugin import events
+from mediadrop.model import User, Group, Category
+import datetime
+import re
+
+import logging
+log = logging.getLogger(__name__)
+
+upload_form = UploadForm(
+    action = url_for(controller='/upload', action='submit'),
+    async_action = url_for(controller='/upload', action='submit_async')
+)
+
+class UploadController(BaseController):
+    """
+    Media Upload Controller
+    """
+
+    def __before__(self, *args, **kwargs):
+        if not request.settings['appearance_enable_user_uploads']:
+            abort(404)
+        result = BaseController.__before__(self, *args, **kwargs)
+        # BareBonesController will set request.perm
+        if not request.perm.contains_permission('upload'):
+            abort(404)
+        return result
+
+    @expose('upload/index.html')
+    @observable(events.UploadController.index)
+    def index(self, **kwargs):
+        """Display the upload form.
+
+        :rtype: Dict
+        :returns:
+            legal_wording
+                XHTML legal wording for rendering
+            support_email
+                An help contact address
+            upload_form
+                The :class:`~mediadrop.forms.uploader.UploadForm` instance
+            form_values
+                ``dict`` form values, if any
+
+        """
+        support_emails = request.settings['email_support_requests']
+        support_emails = email.parse_email_string(support_emails)
+        support_email = support_emails and support_emails[0] or None
+
+        return dict(
+            legal_wording = request.settings['wording_user_uploads'],
+            support_email = support_email,
+            upload_form = upload_form,
+            form_values = kwargs,
+        )
+
+    @expose('json', request_method='POST')
+    @validate(upload_form)
+    @autocommit
+    @observable(events.UploadController.submit_async)
+    def submit_async(self, **kwargs):
+        """Ajax form validation and/or submission.
+
+        This is the save handler for :class:`~mediadrop.forms.media.UploadForm`.
+
+        When ajax is enabled this action is called for each field as the user
+        fills them in. Although the entire form is validated, the JS only
+        provides the value of one field at a time,
+
+        :param validate: A JSON list of field names to check for validation
+        :parma \*\*kwargs: One or more form field values.
+        :rtype: JSON dict
+        :returns:
+            :When validating one or more fields:
+
+            valid
+                bool
+            err
+                A dict of error messages keyed by the field names
+
+            :When saving an upload:
+
+            success
+                bool
+            redirect
+                If valid, the redirect url for the upload successful page.
+
+        """
+        if 'validate' in kwargs:
+            # we're just validating the fields. no need to worry.
+            fields = json.loads(kwargs['validate'])
+            err = {}
+            for field in fields:
+                if field in tmpl_context.form_errors:
+                    err[field] = tmpl_context.form_errors[field]
+
+            data = dict(
+                valid = len(err) == 0,
+                err = err
+            )
+        else:
+            # We're actually supposed to save the fields. Let's do it.
+            if len(tmpl_context.form_errors) != 0:
+                # if the form wasn't valid, return failure
+                tmpl_context.form_errors['success'] = False
+                data = tmpl_context.form_errors
+            else:
+                # else actually save it!
+                kwargs.setdefault('name')
+
+                apply_yourname = kwargs.get('name') or kwargs.get('email') or "To be determined"
+                if re.match(r'^[a-z]+[0-9]{2}$', apply_yourname):
+                    apply_email = apply_yourname + '@student.ssis-suzhou.net'
+                else:
+                    apply_email = apply_yourname + '@ssis-suzhou.net'
+                
+                apply_title = kwargs['title']
+                apply_description = kwargs['description']
+                apply_tags = kwargs['tags']
+                if request.settings.get('upload_default_category') and request.settings.get('upload_default_category'):
+                    default_category = Category.query.filter(Category.name == request.settings['upload_default_category']).first()
+                    apply_categories = [default_category.id] if default_category else None
+                else:
+                    apply_categories = None
+                apply_file = kwargs['file']
+                apply_url = kwargs.get('url')
+
+                media_obj = self.save_media_obj(
+                    apply_yourname, apply_email,
+                    apply_title, apply_description,
+                    apply_tags, apply_categories, apply_file, apply_url,
+                )
+
+                email.send_media_notification(media_obj)
+                data = dict(
+                    success = True,
+                    redirect = url_for(controller='admin/media', action=str(media_obj.id))
+                )
+
+        return data
+
+    @expose(request_method='POST')
+    @validate(upload_form, error_handler=index)
+    @autocommit
+    @observable(events.UploadController.submit)
+    def submit(self, **kwargs):
+        """
+        """
+        kwargs.setdefault('name')
+
+        if request.settings.get('upload_default_category') and request.settings.get('upload_default_category'):
+            default_category = Category.query.filter(Category.name == request.settings['upload_default_category']).first()
+            apply_categories = [default_category.id] if default_category else None
+        else:
+            apply_categories = None
+
+        apply_username = kwargs.get('name') or kwargs.get('email') or "To be determined"
+        if re.match(r'^[a-z]+[0-9]{2}$', apply_username):
+            apply_email = apply_username + '@student.ssis-suzhou.net'
+            default_category = Category.query.filter(Category.name == 'StudentUploads').first()
+        else:
+            apply_email = apply_username + '@ssis-suzhou.net'
+            default_category = Category.query.filter(Category.name == 'TeacherUploads').first()
+
+        # Save the media_obj!
+        media_obj = self.save_media_obj(
+            apply_username, apply_email,
+            kwargs['title'], kwargs['description'],
+            kwargs['tags'], apply_categories, kwargs.get('file'), kwargs.get('url'),
+        )
+        email.send_media_notification(media_obj)
+
+        # Redirect to success page!
+        redirect(controller="admin/media", action=media_obj.id)
+
+    @expose('upload/success.html')
+    @observable(events.UploadController.success)
+    def success(self, **kwargs):
+        return dict()
+
+    @expose('upload/failure.html')
+    @observable(events.UploadController.failure)
+    def failure(self, **kwargs):
+        return dict()
+
+    def save_media_obj(self, name, email, title, description, tags, categories, uploaded_file, url):
+        # create our media object as a status-less placeholder initially
+        media_obj = Media()
+        media_obj.author = Author(name, email)
+        media_obj.title = title
+        media_obj.slug = get_available_slug(Media, title)
+        media_obj.description = description
+        if request.settings['wording_display_administrative_notes']:
+            media_obj.notes = request.settings['wording_administrative_notes']
+        media_obj.set_tags(tags)
+        if categories:
+            media_obj.set_categories(categories)
+
+        # Give the Media object an ID.
+        DBSession.add(media_obj)
+        DBSession.flush()
+
+        # Create a MediaFile object, add it to the media_obj, and store the file permanently.
+        if uploaded_file:
+            media_file = add_new_media_file(media_obj, file=uploaded_file, url=url)
+        else:
+            media_file = None
+        # The thumbs may have been created already by add_new_media_file
+        if not has_thumbs(media_obj):
+            create_default_thumbs_for(media_obj)
+
+        media_obj.update_status()
+        DBSession.flush()
+
+        return media_obj
---
+++ mediadrop/lib/auth/middleware.py
@@ -0,0 +1,296 @@
+# This file is a part of MediaDrop (http://www.mediadrop.net),
+# Copyright 2009-2013 MediaDrop contributors
+# For the exact contribution history, see the git revision log.
+# The source code contained in this file is licensed under the GPLv3 or
+# (at your option) any later version.
+# See LICENSE.txt in the main project directory, for more information.
+
+import re
+
+from repoze.who.classifiers import default_challenge_decider, default_request_classifier
+from repoze.who.middleware import PluggableAuthenticationMiddleware
+from repoze.who.plugins.auth_tkt import AuthTktCookiePlugin
+from repoze.who.plugins.friendlyform import FriendlyFormPlugin
+from repoze.who.plugins.sa import SQLAlchemyAuthenticatorPlugin
+from mediadrop.config.routing import login_form_url, login_handler_url, \
+    logout_handler_url, post_login_url, post_logout_url
+from pylons.controllers.util import Request
+
+from mediadrop.lib.auth.permission_system import MediaDropPermissionSystem
+
+from mediadrop.model import User, Group
+import imaplib
+import ldap
+import re
+import datetime
+from mediadrop.model.meta import DBSession
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
+from pylons import config as pylonsconfig
+
+__all__ = ['add_auth', 'classifier_for_flash_uploads']
+
+class GeneralAuth(object):
+    def __init__(self, config):
+        """ Give this object whatever keys are in config: host, etc """
+        self.__dict__.update(config)
+
+        restricted_group_name = "RestrictedGroup"
+        self.restricted_group = DBSession.query(Group).filter(Group.group_name.in_([restricted_group_name])).first()
+        self.builtin_editor_group = DBSession.query(Group).filter(Group.group_id.in_([2])).first()
+
+        if not self.restricted_group:
+            make_new_group = Group(name=restricted_group_name, display_name=restricted_group_name)
+            # Copy the permissions from the same group that can give us access to the /admin section
+            from copy import copy
+            make_new_group.permissions = copy(builtin_editor_group.permissions)
+            DBSession.add(make_new_group)
+            DBSession.commit()
+            # get the group we just created
+            self.restricted_group = DBSession.query(Group).filter(Group.group_name.in_([restricted_group_name])).first()
+
+    def __del__(self):
+        self.delete()
+
+    def auth(self, username, password):
+        if not hasattr(self, 'connection'):
+            # lazy connection
+            self.connection = self.init()
+        return bool(self.login(username, password))
+
+    def log(self, exception):
+        #TODO: Implement
+        print(str(exception))
+
+    def init(self):
+        """ Should return the connection object """
+        raise NotImplemented
+
+    def delete(self):
+        """ Should free self.connection """
+        raise NotImplemented
+
+    def login(self, username, password):
+        """ Return bool whether or not user username with password authenticates """
+        raise NotImplemented
+
+    def default_domain(self):
+        """ Return the email domain these accounts should use """
+        raise NotImplemented
+
+    def default_groups(self):
+        """ Return list of group(s) these accounts should be in """
+        raise NotImplemented
+
+class LDAPAuthentication(GeneralAuth):
+
+    def init(self):
+        trace_level = 1 if hasattr(self, 'trace_level') and self.trace_level else 0
+        print(trace_level)
+        return ldap.initialize(self.host, trace_level=trace_level)
+
+    def delete(self):
+        self.connection.unbind()
+
+    def login(self, username, password):
+        try:
+            return self.connection.simple_bind_s("{cnword}={uid},{ouphrase},{dcphrase}".format(
+                cnword=self.cnword, uid=username,
+                ouphrase=self.ouphrase, dcphrase=self.dcphrase), password)
+        except Exception, e:
+            self.log(e)
+            return False
+
+    def default_domain(self):
+        return self.default_email_domain if hasattr(self, 'default_email_domain') else "example.org"
+
+    def default_groups(self):
+        return [self.builtin_editor_group]
+
+class IMAPAuthentication(GeneralAuth):
+
+    def init(self):
+        if not hasattr(self, 'connection'):
+            return None
+        return imaplib.IMAP4_SSL(self.host)
+
+    def delete(self):
+        pass
+
+    def login(self, username, password):
+        self.connection = self.init()
+        try:
+            authenticated = self.connection.login(username, password)
+        except Exception, e:
+            self.log(e)
+            return False
+        if authenticated:
+            print("logging out")
+            self.connection.logout()
+        print("Authenticated?: ", authenticated)
+        self.connection = None  # release memory for this connection
+        return authenticated
+
+    def default_domain(self):
+        return self.host
+
+    def default_groups(self):
+        return [self.restricted_group]
+
+class MediaDropAuthenticatorPlugin(SQLAlchemyAuthenticatorPlugin):
+    def __init__(self, config, *args, **kwargs):
+        super(SQLAlchemyAuthenticatorPlugin, self).__init__(*args, **kwargs)
+        pylonsconfig['imap'] = IMAPAuthentication(config['imap'])
+        pylonsconfig['ldap'] = LDAPAuthentication(config['ldap'])
+
+    def authenticate(self, environ, identity, notagain=False):
+        login = super(MediaDropAuthenticatorPlugin, self).authenticate(environ, identity)
+        if login is None:
+            if notagain:
+                return None   # prevent infinite loop
+
+            username = identity['login']
+            password = identity['password']
+
+            if re.match(r'^[a-z]+[0-9]{2}$', username):
+                auth_to_use = pylonsconfig['imap']
+            else:
+                auth_to_use = pylonsconfig['ldap']
+                
+            if not auth_to_use.auth(username, password):
+                return None
+            else:
+                # Use the model to create the user which automagically gets put in the database
+                user = User()
+                user.display_name = username
+                user.user_name = username
+                user.email_address = "{}@{}".format(user.user_name, auth_to_use.default_domain())
+                user.password = u'uselesspassword#%^^#@'
+                user.groups = auth_to_use.default_groups()
+
+                try:
+                    #actually add the user
+                    DBSession.add(user)
+                    DBSession.commit()
+                except IntegrityError, e:
+                    DBSession.rollback()
+                    return None
+                except InvalidRequestError, e:
+                    new_user = DBSession.merge(user)
+                    DBSession.add(new_user)
+                    DBSession.commit()
+                except Exception, e:
+                    DBSession.rollback()
+                    return None
+
+                # Now repoze.who should be able to login
+                return self.authenticate(environ, identity, notagain=True)
+
+        user = self.get_user(login)
+        # The return value of this method is used to identify the user later on.
+        # As the username can be changed, that's not really secure and may 
+        # lead to confusion (user is logged out unexpectedly, best case) or 
+        # account take-over (impersonation, worst case).
+        # The user ID is considered constant and likely the best choice here.
+        return user.id
+    
+    @classmethod
+    def by_attribute(cls, config, attribute_name=None):
+        from mediadrop.model import DBSession, User
+        authenticator = MediaDropAuthenticatorPlugin(config, User, DBSession)
+        if attribute_name:
+            authenticator.translations['user_name'] = attribute_name
+        return authenticator
+
+
+class MediaDropCookiePlugin(AuthTktCookiePlugin):
+    def __init__(self, secret, **kwargs):
+        if kwargs.get('userid_checker') is not None:
+            raise TypeError("__init__() got an unexpected keyword argument 'userid_checker'")
+        kwargs['userid_checker'] = self._check_userid
+        super(MediaDropCookiePlugin, self).__init__(secret, **kwargs)
+    
+    def _check_userid(self, user_id):
+        # only accept numeric user_ids. In MediaCore < 0.10 the cookie contained
+        # the user name, so invalidate all these old sessions.
+        if re.search('[^0-9]', user_id):
+            return False
+        return True
+
+
+def who_args(config):
+    auth_by_username = MediaDropAuthenticatorPlugin.by_attribute(config, 'user_name')
+    
+    form = FriendlyFormPlugin(
+        login_form_url,
+        login_handler_url,
+        post_login_url,
+        logout_handler_url,
+        post_logout_url,
+        rememberer_name='cookie',
+        charset='utf-8',
+    )
+    cookie_secret = config['sa_auth.cookie_secret']
+    seconds_30_days = 30*24*60*60 # session expires after 30 days
+    cookie = MediaDropCookiePlugin(cookie_secret, 
+        cookie_name='authtkt', 
+        timeout=seconds_30_days, # session expires after 30 days
+        reissue_time=seconds_30_days/2, # reissue cookie after 15 days
+    )
+    
+    who_args = {
+        'authenticators': [
+            ('auth_by_username', auth_by_username)
+        ],
+        'challenge_decider': default_challenge_decider,
+        'challengers': [('form', form)],
+        'classifier': classifier_for_flash_uploads,
+        'identifiers': [('main_identifier', form), ('cookie', cookie)],
+        'mdproviders': [],
+    }
+    return who_args
+
+
+def authentication_middleware(app, config):
+    return PluggableAuthenticationMiddleware(app, **who_args(config))
+
+
+class AuthorizationMiddleware(object):
+    def __init__(self, app, config):
+        self.app = app
+        self.config = config
+    
+    def __call__(self, environ, start_response):
+        environ['mediadrop.perm'] = \
+            MediaDropPermissionSystem.permissions_for_request(environ, self.config)
+        return self.app(environ, start_response)
+
+
+def add_auth(app, config):
+    authorization_app = AuthorizationMiddleware(app, config)
+    return authentication_middleware(authorization_app, config)
+
+
+def classifier_for_flash_uploads(environ):
+    """Normally classifies the request as browser, dav or xmlpost.
+
+    When the Flash uploader is sending a file, it appends the authtkt session
+    ID to the POST data so we spoof the cookie header so that the auth code
+    will think this was a normal request. In the process, we overwrite any
+    pseudo-cookie data that is sent by Flash.
+
+    TODO: Currently overwrites the HTTP_COOKIE, should ideally append.
+    """
+    classification = default_request_classifier(environ)
+    if classification == 'browser' \
+    and environ['REQUEST_METHOD'] == 'POST' \
+    and 'Flash' in environ.get('HTTP_USER_AGENT', ''):
+        session_key = environ['repoze.who.plugins']['cookie'].cookie_name
+        # Construct a temporary request object since this is called before
+        # pylons.request is populated. Re-instantiation later comes cheap.
+        request = Request(environ)
+        try:
+            session_id = str(request.POST[session_key])
+            environ['HTTP_COOKIE'] = '%s=%s' % (session_key, session_id)
+        except (KeyError, UnicodeEncodeError):
+            pass
+    return classification
